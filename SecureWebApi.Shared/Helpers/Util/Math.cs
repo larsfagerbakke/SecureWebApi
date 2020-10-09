d6ba3077f13b9aef6802142f90ffe2ec1a9df151
Lars Fagerbakke <lars@fagerbakke.net>
2020-10-09 22:24:28 +0200

    Added helper method to generate number between

---
+++ SecureWebApi.Shared/Helpers/Util/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecureWebApi.Shared.Helpers.Util
+{
+    public class Math
+    {
+        private static Random rnd = new Random();
+
+        public static int GenerateNumberBetween(int min, int max)
+        {
+            return min + (int)((max - min) * rnd.NextDouble());
+        }
+    }
+}
