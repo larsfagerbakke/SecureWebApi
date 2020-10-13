e18be2e68513721bd7efba4b8c8e0870d118f66b
Lars Fagerbakke <lars@fagerbakke.net>
2020-10-13 22:24:46 +0200

    Rectatored to use static random

--- SecureWebApi.Shared/Helpers/Util/Math.cs
+++ SecureWebApi.Shared/Helpers/Util/Math.cs
@@ -6,11 +6,9 @@ namespace SecureWebApi.Shared.Helpers.Util
 {
     public class Math
     {
-        private static Random rnd = new Random();
-
         public static int GenerateNumberBetween(int min, int max)
         {
-            return min + (int)((max - min) * rnd.NextDouble());
+            return min + (int)((max - min) * Util.Random.NextDouble());
         }
     }
 }
