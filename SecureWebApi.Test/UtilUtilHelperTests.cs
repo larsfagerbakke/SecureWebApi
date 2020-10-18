005adf087b8c533ab1c8b2d3ce16efe7c172acb2
Lars Fagerbakke <lars@fagerbakke.net>
2020-10-18 23:17:26 +0200

    Actually added a unit test

---
+++ SecureWebApi.Test/UtilUtilHelperTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecureWebApi.Tests
+{
+    [TestClass]
+    public class UtilUtilHelperTests
+    {
+        [TestMethod]
+        public void GenerateRandomString_Test()
+        {
+            var ret = SecureWebApi.Shared.Helpers.Util.Util.GenerateRandomString(10);
+
+            Assert.AreEqual(ret.Length, 10);
+        }
+    }
+}
