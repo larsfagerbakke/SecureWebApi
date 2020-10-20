15d6cf6809c13a1fc465cbbcf86c91eda6b34353
Lars Fagerbakke <lars@fagerbakke.net>
2020-10-20 23:24:10 +0200

    Added interface for SMSService

---
+++ SecureWebApi.Shared/Services/ISMSService.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace SecureWebApi.Shared.Services
+{
+    public interface ISMSServiceConfiguration
+    {
+
+    }
+
+    public interface ISMSService
+    {
+        Task<bool> Send(string receiver, string sender, string message);
+    }
+
+    public abstract class SMSService : ISMSService
+    {
+        private SMSService() { }
+
+        public SMSService(ISMSServiceConfiguration config) { }
+
+        public abstract Task<bool> Send(string receiver, string sender, string message);
+    }
+}
